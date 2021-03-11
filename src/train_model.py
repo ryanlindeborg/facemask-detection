09138841749eb6b997cbb39f6549e8ebc829b9e2
DXcarlos <clopezh@uni.pe>
2021-03-10 20:46:47 -0500

    Keep only lines to train model

--- src/train_model.py
+++ src/train_model.py
@@ -1,13 +1,13 @@
+import numpy as np
 import cv2
 import os
-import numpy as np
-from keras.utils import np_utils
-from keras.models import Sequential
+
 from keras.layers import Dense, Activation, Dropout, Conv2D, Flatten, MaxPooling2D
 from keras.callbacks import ModelCheckpoint
+from keras.models import Sequential
+from keras.utils import np_utils
+
 from sklearn.model_selection import train_test_split
-from matplotlib import pyplot as plt
-from keras.models import load_model
 
 # Use the file path where your dataset is stored
 data_path = './data/'
@@ -68,41 +68,10 @@ model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc']
 model.summary()
 
 # Split data in train and test
-train_data, test_data, train_target, test_target = train_test_split(data, target, test_size=0.1)
+train_data, test_data, train_target, test_target = train_test_split(data, target, test_size=0.1, random_state=42)
 
 # Train model and save checkpoint
 checkpoint = ModelCheckpoint('./model_checkpoints/model-{epoch:03d}.model', monitor='val_loss', verbose=0,
                              save_best_only=True, mode='auto')
-history = model.fit(train_data, train_target, epochs=20, callbacks=[checkpoint], validation_split=0.2)
-
-# Load the best model
-model = load_model('./model/model_checkpoints/model-005.model')
-faceCascade = cv2.CascadeClassifier('./opencv/data/HaarCascade/haarcascade_frontalface_default.xml')
-video_capture = cv2.VideoCapture(0)
-labels_dict = {0: 'NO MASK', 1: 'MASK'}
-color_dict = {0: (0, 0, 255), 1: (0, 255, 0)}
-
-while True:
-    ret, frame = video_capture.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(gray, 1.3, 5)
-
-    for x, y, w, h in faces:
-        face_img = gray[y:y + w, x:x + h]
-        resized = cv2.resize(face_img, (img_size, img_size))
-        normalized = resized / 255.0
-        reshaped = np.reshape(normalized, (1, img_size, img_size, 1))
-        result = model.predict(reshaped)
-
-        label = np.argmax(result, axis=1)[0]
-        cv2.rectangle(frame, (x, y), (x + w, y + h), color_dict[label], 2)
-        cv2.putText(frame, labels_dict[label], (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2)
-
-    cv2.imshow('Video', frame)
-    key = cv2.waitKey(1)
-
-    if key == 27:
-        break
-
-cv2.destroyAllWindows()
-video_capture.release()
+history = model.fit(train_data, train_target, epochs=10, callbacks=[checkpoint], validation_split=0.2,
+                    use_multiprocessing=True, workers=12)
