ca4df3a910f11cd143e9c9e46ff2033d5554fb3f
DXcarlos <clopezh@uni.pe>
2021-03-10 20:47:02 -0500

    Test model on webcam with opencv

---
+++ src/test_model.py
@@ -0,0 +1,38 @@
+import numpy as np
+import cv2
+
+from keras.models import load_model
+
+img_size = 150
+
+# Load the best model
+model = load_model('./model_checkpoints/model-005.model')
+faceCascade = cv2.CascadeClassifier('./utils/haarcascade_frontalface_default.xml')
+video_capture = cv2.VideoCapture(0)
+labels_dict = {0: 'NO MASK', 1: 'MASK'}
+color_dict = {0: (0, 0, 255), 1: (0, 255, 0)}
+
+while True:
+    ret, frame = video_capture.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(gray, 1.3, 5)
+
+    for x, y, w, h in faces:
+        face_img = gray[y:y + w, x:x + h]
+        resized = cv2.resize(face_img, (img_size, img_size))
+        normalized = resized / 255.0
+        reshaped = np.reshape(normalized, (1, img_size, img_size, 1))
+        result = model.predict(reshaped)
+
+        label = np.argmax(result, axis=1)[0]
+        cv2.rectangle(frame, (x, y), (x + w, y + h), color_dict[label], 2)
+        cv2.putText(frame, labels_dict[label], (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2)
+
+    cv2.imshow('Video', frame)
+    key = cv2.waitKey(1)
+
+    if key == 27:
+        break
+
+cv2.destroyAllWindows()
+video_capture.release()
