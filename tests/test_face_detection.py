4700136fa5d7acd6027d50e9e308484422c2be06
DXcarlos <clopezh@uni.pe>
2021-04-06 21:39:31 -0500

    Modify functions to improve performance of face detection

--- tests/test_face_detection.py
+++ tests/test_face_detection.py
@@ -1,19 +1,20 @@
 import cv2
 import logging
 
-from face_detection import detect_faces_and_locations
+from face_detection import detect_faces_and_locations, load_face_model
 from utils import draw_bounding_boxes_and_confidences
+# from imutils.video import FileVideoStream
 
 logger = logging.getLogger(__name__)
 
 
-def display_video(video_source, model_name):
+def display_video(video_source, face_net, model_type):
     """Display video with opencv"""
 
     ret, frame = video_source.read()
 
     if ret:
-        faces, locs, confidences = detect_faces_and_locations(frame, model_name)
+        faces, locs, confidences = detect_faces_and_locations(frame, face_net, model_type)
         frame = draw_bounding_boxes_and_confidences(frame, locs, confidences)
         cv2.imshow("Frame", frame)
 
@@ -24,12 +25,13 @@ def display_video(video_source, model_name):
     return key
 
 
-def test_face_detection_img(img_path, model_name, show_individual_faces=False):
+def test_face_detection_img(img_path, model_type, show_individual_faces=False):
     """Test face detection model in images"""
 
-    logger.info(f"Starting face detection in image with model {model_name}")
+    logger.info(f"Starting face detection in image with model {model_type}")
     image = cv2.imread(img_path)
-    faces, locs, confidences = detect_faces_and_locations(image, model_name)
+    face_net = load_face_model(model_type)
+    faces, locs, confidences = detect_faces_and_locations(image, face_net, model_type)
     image = draw_bounding_boxes_and_confidences(image, locs, confidences, False)
     cv2.imshow("Image", image)
 
@@ -41,25 +43,27 @@ def test_face_detection_img(img_path, model_name, show_individual_faces=False):
     cv2.destroyAllWindows()
 
 
-def test_face_detection_video(model_name, source_type='webcam', video_path=None):
+def test_face_detection_video(model_type, source_type='webcam', video_path=None):
     """Test face detection model in real time with webcam or from video"""
 
-    logger.info(f"Starting face detection in webcam with model {model_name}")
+    logger.info(f"Starting face detection in webcam with model {model_type}")
 
+    face_net = load_face_model(model_type)
     source = 0 if source_type == 'webcam' else video_path
     video_source = cv2.VideoCapture(source)
 
     if source_type == 'webcam':
 
         while True:
-            key = display_video(video_source, model_name)
+            key = display_video(video_source, face_net, model_type)
             if key == ord("q"):
                 break
 
     else:
 
+        # video_source = FileVideoStream(source)
         while video_source.isOpened():
-            key = display_video(video_source, model_name)
+            key = display_video(video_source, face_net, model_type)
             if key == ord("q"):
                 break
 
@@ -69,11 +73,11 @@ def test_face_detection_video(model_name, source_type='webcam', video_path=None)
 
 def main():
     logging.basicConfig(format='%(asctime)s :: %(levelname)s :: %(name)s  :: %(message)s', level=logging.INFO)
-    test_face_detection_video(model_name='caffe', source_type='video',
-                              video_path='./data/face_detection_data/ig_pollosus_mask_1.mp4')
-    test_face_detection_img('./data/face_detection_data/group_4.jpg', model_name='caffe')
-    test_face_detection_img('./data/face_detection_data/group_3.jpg', model_name='mtcnn')
-    test_face_detection_video(model_name='caffe', source_type='webcam')
+    test_face_detection_video(model_type='caffe', source_type='webcam')
+    test_face_detection_video(model_type='caffe', source_type='video',
+                              video_path='./data/face_detection_data/ig_pollosus_mask_3.mp4')
+    test_face_detection_img('./data/face_detection_data/group_4.jpg', model_type='caffe')
+    test_face_detection_img('./data/face_detection_data/group_3.jpg', model_type='mtcnn')
 
 
 if __name__ == "__main__":
